5cf4b37ab2cf15e8bccfb5be0a7559db723a3ef8
12Aleks <tanais2010@yahoo.com>
2021-02-14 12:56:15 +0100

    added context

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AlertContext} from "../context/alert/alertContext";
 
-export const Alert = ({alert}) => {
-
+export const Alert = () => {
+  const {alert, hide} = useContext(AlertContext);
     if(!alert){
         return null
     }
@@ -10,7 +11,7 @@ export const Alert = ({alert}) => {
         <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`} >
             <strong>Holy guacamole!</strong>
             {alert.text}
-            <button type="button" className="close" aria-label="Close">
+            <button onClick={hide} type="button" className="close" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
